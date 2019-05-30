d2da1806b86d8fc32a44eef62897b803f189b5da
AriefRachman99 <mariefrachmanm@gmail.com>
2019-05-30 14:36:51 -0700

    registrasi fixed

--- android/PuskesmasSumbersari/app/src/main/java/com/example/puskesmassumbersari/MainActivity.java
+++ android/PuskesmasSumbersari/app/src/main/java/com/example/puskesmassumbersari/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.puskesmassumbersari.controllers.LoginActivity;
--- android/PuskesmasSumbersari/app/src/main/java/com/example/puskesmassumbersari/config/Server.java
+++ android/PuskesmasSumbersari/app/src/main/java/com/example/puskesmassumbersari/config/Server.java
@@ -2,10 +2,10 @@ package com.example.puskesmassumbersari.config;
 
 public class Server {
 
-    public static final String URL = "http://192.168.1.19/onque/";
+    // public static final String URL = "http://192.168.1.19/onque/";
     //public static final String URL = "http://10.10.3.3/onque/";
 
     //public static final String URL = "http://192.168.1.13/onque/";
     //public static final  String URL = "http://10.10.3.76/onque/";
-    //public static  final String URL = "http://192.168.56.1/onque/";
+    public static  final String URL = "http://192.168.56.1/onque/";
 }
