ce57891ae970adb1540290da7be875a64f008326
Re <47809253+redrgnl@users.noreply.github.com>
2019-06-30 17:57:00 +0700

    change button laporan

--- web/application/views/admin/content/man_laporan.php
+++ web/application/views/admin/content/man_laporan.php
@@ -1,18 +1,12 @@
 <div class="row">
   <div class="col-md-3 col-sm-6 mb-3">
     <div class="card text-white o-hidden h-100 bg-warning">
-      <div class="card-body">
+      <a class="card-body text-white clearfix z-1" style="text-decoration: none" href="<?php echo base_url('laporan/laporan_bulanan')?>">
         <div class="card-body-icon">
           <i class="fas fa-fw fa-user"></i>
         </div>
         <div class="mr-5">Laporan Bulanan</div>
-      </div> 
-      <a class="card-footer text-white clearfix small z-1" href="<?php echo base_url('laporan/laporan_bulanan')?>">
-        <span class="float-left">Selengkapnya</span>
-        <span class="float-right">
-          <i class="fas fa-angle-right"></i>
-        </span>
-      </a>
+      </a> 
     </div>
   </div>
 </div>
--- web/application/views/admin/content/man_laporan_bulanan.php
+++ web/application/views/admin/content/man_laporan_bulanan.php
@@ -1,17 +1,11 @@
 <div class="row">
   <div class="col-md-3 col-sm-6 mb-3">
     <div class="card text-white o-hidden h-100" style="background-color: orange">
-      <div class="card-body">
+      <a class="card-body text-white clearfix z-1" style="text-decoration: none" href="<?php echo base_url('laporan')?>">
         <div class="card-body-icon">
           <i class="fas fa-fw fa-user"></i>
         </div>
         <div class="mr-5">Laporan Hari Ini</div>
-      </div> 
-      <a class="card-footer text-white clearfix small z-1" href="<?php echo base_url('laporan')?>">
-        <span class="float-left">Selengkapnya</span>
-        <span class="float-right">
-          <i class="fas fa-angle-right"></i>
-        </span>
       </a>
     </div>
   </div>
